77770f09f06cc6da63312747419ab8d9a7123c37
Andreas Ölund <andreas@dartvoid.com>
2014-07-28 22:33:08 +0200

    Set build path as target

--- server/server.dart
+++ server/server.dart
@@ -1,7 +1,7 @@
 import 'package:force/force_serverside.dart';
 
 void main() {
-  ForceServer fs = new ForceServer(clientFiles: "../client/web/",
+  ForceServer fs = new ForceServer(clientFiles: "../client/build/web/",
       startPage: "index.html");
 
   fs.setupConsoleLog();
